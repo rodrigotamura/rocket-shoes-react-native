a6a415674e477047bec4b726c3248db4e14b049a
Rodrigo Tamura <rodrigotamura@hotmail.com>
2019-09-12 15:43:25 -0300

    Routes implementation.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './config/ReactotronConfig';
+
+import { View } from 'react-native';
+import Routes from './routes';
+
+// import { Container } from './styles';
+
+const App = () => {
+  return <Routes />;
+};
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#000',
+        },
+      },
+    }
+  )
+);
+
+export default Routes;
